b2cb9978c3b2cfd37a52b65790f36d9f1bec01d2
Benedict Dube <benedict.dube@vcontractor.co.za>
2022-04-22 15:42:23 +0200

    Added environment variables file and renamed the entry point file to index.js as that is the standard across other node projects

--- src/server/index.js
+++ src/server/index.js
@@ -5,9 +5,13 @@ const PORT = process.env.SERVER_PORT || 5000;
 const server = express();
 
 server.get("/", (request, response) => {
-    response.status(200);
-    response.send("Success");
+    response
+        .status(200)
+        .json({});
 });
+
+//TODO: Add error handling for catch all
+server.all("*", () => {});
 
 server.listen(PORT, () => {
     console.log(`Server is running and listening on port: ${PORT}`);
